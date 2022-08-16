d92950c8d37386f1d4d5180bbbfb078ff09c44e2
NishantSambyal <nishantsambyal123@gmail.com>
2022-08-16 10:53:08 +0530

    Login done

--- Login.js
+++ Login.js
@@ -10,34 +10,80 @@ import {
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      resApi: '',
+      login: undefined,
+      password: undefined,
+    };
   }
+
+  fetchApi = () => {
+    const log = this.state.login;
+    const pass = this.state.password;
+
+    if (log && pass) {
+      fetch('https://reactnative.dev/movies.json', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then(res => {
+          this.setState({resApi: res});
+          console.log(res);
+        })
+        .catch(error => console.log(error));
+    } else {
+      alert('Please enter email and password');
+    }
+  };
+
+  onEmailTextChange = emailString => {
+    this.setState({login: emailString});
+  };
+
+  onPassTextChange = passString => {
+    this.setState({password: passString});
+  };
 
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.labelView}>
-          <Text style={styles.label}>Email</Text>
-        </View>
-        <View style={styles.inputView}>
-          <TextInput style={styles.TextInput} />
-        </View>
-        <View style={styles.labelView}>
-          <Text style={styles.label}>Password</Text>
-        </View>
-        <View style={styles.inputView}>
-          <TextInput style={styles.TextInput} />
-        </View>
+        <View style={styles.cardView}>
+          <View style={styles.labelView}>
+            <Text style={styles.label}>Email</Text>
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              onChangeText={this.onEmailTextChange}
+            />
+          </View>
+          <View style={styles.labelView}>
+            <Text style={styles.label}>Password</Text>
+          </View>
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.TextInput}
+              onChangeText={this.onPassTextChange}
+            />
+          </View>
 
-        <TouchableOpacity>
-          <Text style={styles.forgot_button}>Forgot Password?</Text>
-        </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.forgot_button}>Forgot Password?</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.loginBtn}
-          onPress={() => this.props.navigation.navigate('List')}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.loginBtn}
+            onPress={() => this.fetchApi()}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+          <Text style={styles.loginText}>
+            {JSON.stringify(this.state.resApi?.title)}
+          </Text>
+        </View>
       </View>
     );
   }
@@ -86,5 +132,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 40,
     backgroundColor: '#ffff00',
+  },
+  cardView: {
+    shadowColor: 'gray',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 6,
+    marginVertical: 10,
+    backgroundColor: '#fff',
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    borderRadius: 4,
+    width: '80%',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
